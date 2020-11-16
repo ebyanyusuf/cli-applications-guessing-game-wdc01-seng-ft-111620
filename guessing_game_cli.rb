cdbcd656dca3ade3314791a63b4d1ad9ebd6cae0
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-16 05:13:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,14 +1,13 @@
 def run_guessing_game
-     number = rand(1..6).to_s
-     prompt_user = (user_input = gets.chomp)
+   random_number = rand(6) + 1
+   user_input = gets.chomp
+   if user_input == random_number.to_s
+     puts "You guessed the correct number!" 
+   elsif user_input == "exit"
+     puts "Goodbye!"
 
-     if user_input == "exit"
-         puts "Goodbye!"
-     end
+   else
+     puts "Sorry! The computer guessed #{random_number}."
+   end 
 
-     if user_input == number
-         puts "You guessed the correct number!"
-     elsif user_input != number
-         puts "Sorry! The computer guessed 6."
-     end
  end
